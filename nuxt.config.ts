ec22343c81b839260eae38201cef590b8b75dcf3
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-20 17:02:52 +0700

    Add @nuxtjs/tailwindcss module to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  },
 })
