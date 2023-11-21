47c8b1e8632164be2a1649df45e92b29f7880b77
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-21 17:55:59 +0700

    Update AppFooter styles

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -6,7 +6,7 @@
       >
         <div class="md:flex">
           <div>
-            <h2 class="text-2xl font-bold">Get in touch</h2>
+            <h2 class="text-2xl font-bold text-white">Get in touch</h2>
             <p
               class="text-base text-gray-100 py-4 leading-8 w-72 hover:animate-pulse"
             >
@@ -14,8 +14,8 @@
             </p>
           </div>
           <div class="">
-            <h2 class="text-2xl font-semibold">Connect</h2>
-            <ul class="grid leading-8 py-4 text-base">
+            <h2 class="text-2xl font-semibold text-white">Connect</h2>
+            <ul class="grid leading-8 py-4 text-base text-white">
               <NuxtLink to="" class="hover:animate-pulse">LinkedIn</NuxtLink>
               <NuxtLink to="" class="hover:animate-pulse">Discord</NuxtLink>
               <NuxtLink to="" class="hover:animate-pulse">Instagram</NuxtLink>
@@ -25,15 +25,15 @@
         </div>
         <div class="md:flex gap-10">
           <div>
-            <h2 class="text-2xl font-semibold">Blog</h2>
-            <ul class="grid leading-8 py-4 text-base">
+            <h2 class="text-2xl font-semibold text-white">Blog</h2>
+            <ul class="grid leading-8 py-4 text-base text-white">
               <NuxtLink to="" class="hover:animate-pulse">Nuxt 3</NuxtLink>
               <NuxtLink to="" class="hover:animate-pulse">Vue 3</NuxtLink>
             </ul>
           </div>
         </div>
       </div>
-      <div class="bg-primary-500">
+      <div class="dark:bg-primary-500 bg-primary-600">
         <p class="text-center text-gray-100 py-4">
           &copy; 2023 rstubryan. All rights reserved.
         </p>
