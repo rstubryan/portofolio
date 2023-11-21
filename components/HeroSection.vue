de4cd943dc983ed137e3cae52beebe1fac29074d
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-21 15:59:48 +0700

    Update assets img dir and wip hero

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <div class="container mx-auto md:px-0 px-8 md:py-64 py-8" id="hero">
-      <div>
+      <div class="">
+        <h3 class="text-primary font-bold text-2xl py-1">
+          Front<span class="text-black dark:text-white">-</span>End
+          <span class="text-black dark:text-white">Developer</span>
+        </h3>
         <h2 class="container md:text-4xl text-3xl font-semibold w-96">
           Hello, I'm
           <span class="text-primary font-bold">Restu</span> Bumi Ryan Ramadhan
-          <p class="text-base text-gray-600 dark:text-gray-400 w-[300px] md:w-[630px] py-4 leading-8">
+          <p
+            class="text-base text-gray-600 dark:text-gray-400 w-[300px] md:w-[630px] py-4 leading-8"
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam a
             nemo sequi qui ad fuga magni. Commodi animi laudantium eligendi hic
             quam perspiciatis odio aperiam, itaque nisi consequatur dignissimos
