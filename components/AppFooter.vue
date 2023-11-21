d68d7fc63b5d3b98885e1bf5df6de5114dee5fd9
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-21 17:52:40 +0700

    Update AppFooter.vue with contact information and social media links

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,3 +1,43 @@
 <template>
-  <div></div>
+  <div>
+    <div class="bg-primary">
+      <div
+        class="container mx-auto md:h-72 h-[500px] md:py-14 py-8 p-8 md:p-0 md:flex md:justify-between"
+      >
+        <div class="md:flex">
+          <div>
+            <h2 class="text-2xl font-bold">Get in touch</h2>
+            <p
+              class="text-base text-gray-100 py-4 leading-8 w-72 hover:animate-pulse"
+            >
+              restubumirr@gmail.com
+            </p>
+          </div>
+          <div class="">
+            <h2 class="text-2xl font-semibold">Connect</h2>
+            <ul class="grid leading-8 py-4 text-base">
+              <NuxtLink to="" class="hover:animate-pulse">LinkedIn</NuxtLink>
+              <NuxtLink to="" class="hover:animate-pulse">Discord</NuxtLink>
+              <NuxtLink to="" class="hover:animate-pulse">Instagram</NuxtLink>
+              <NuxtLink to="" class="hover:animate-pulse">Facebook</NuxtLink>
+            </ul>
+          </div>
+        </div>
+        <div class="md:flex gap-10">
+          <div>
+            <h2 class="text-2xl font-semibold">Blog</h2>
+            <ul class="grid leading-8 py-4 text-base">
+              <NuxtLink to="" class="hover:animate-pulse">Nuxt 3</NuxtLink>
+              <NuxtLink to="" class="hover:animate-pulse">Vue 3</NuxtLink>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="bg-primary-500">
+        <p class="text-center text-gray-100 py-4">
+          &copy; 2023 rstubryan. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
