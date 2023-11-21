22a3a2016ca8bfb227b7a9cd87cc6450aaa87b16
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-21 22:39:39 +0700

    Fix responsive layout position

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -4,7 +4,7 @@
       class="container mx-auto md:px-0 px-8 md:py-[230px] py-8 md:flex"
       id="hero"
     >
-      <div class="">
+      <div class="z-50">
         <div>
           <h3 class="font-bold text-2xl py-4">Hello!</h3>
           <h2 class="md:text-6xl text-3xl font-semibold md:w-[700px]">
@@ -22,29 +22,29 @@
               <span class="text-primary">Nuxt 3 & Tailwind CSS</span>.
             </p>
           </h2>
-          <div class="py-4">
-            <UButton
-              icon="i-heroicons-arrow-down"
-              size="xl"
-              color="primary"
-              variant="solid"
-              label="Let's Go!"
-              :trailing="false"
-            />
-          </div>
+        </div>
+        <div class="py-4">
+          <UButton
+            icon="i-heroicons-arrow-down"
+            size="xl"
+            color="primary"
+            variant="solid"
+            label="Let's Go!"
+            :trailing="false"
+          />
         </div>
       </div>
       <div class="mx-auto md:flex md:py-0 py-4">
         <div class="flex items-center justify-center px-16">
           <div class="relative w-full max-w-lg">
             <div
-              class="absolute -bottom-52 -right-48 w-72 h-72 bg-primary-500 rounded-full mix-blend-multiply filter blur-xl dark:opacity-30 opacity-70 animate-blob"
+              class="absolute md:-bottom-52 -bottom-80 md:-right-48 right-32 w-72 h-72 bg-primary-500 rounded-full mix-blend-multiply filter blur-xl dark:opacity-30 opacity-70 animate-blob"
             ></div>
             <div
-              class="absolute top-0 left-72 w-72 h-72 bg-primary-300 rounded-full mix-blend-multiply filter blur-xl dark:opacity-30 opacity-70 animate-blob animation-delay-2000"
+              class="absolute md:top-0 -bottom-96 md:left-72 w-72 h-72 bg-primary-300 rounded-full mix-blend-multiply filter blur-xl dark:opacity-30 opacity-70 animate-blob animation-delay-2000"
             ></div>
             <div
-              class="absolute bottom-8 left-72 w-72 h-72 bg-primary-400 rounded-full mix-blend-multiply filter blur-xl dark:opacity-30 opacity-70 animate-blob animation-delay-4000"
+              class="absolute md:-top-72 -top-10 md:left-72 -right-20 w-72 h-72 bg-primary-400 rounded-full mix-blend-multiply filter blur-xl dark:opacity-30 opacity-70 animate-blob animation-delay-4000"
             ></div>
           </div>
         </div>
@@ -52,7 +52,7 @@
           <NuxtImg
             src="https://avatars.githubusercontent.com/u/96672455?v=4"
             alt="Restu Bumi Ryan Ramadhan"
-            class="rounded-full md:w-[420px] w-[320px] border-8 border-primary"
+            class="rounded-full md:w-[420px] w-[320px] border-8 border-primary z-50"
           />
         </div>
       </div>
