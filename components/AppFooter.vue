20831772b87a8574b65e1b671093e6cf39cfb96f
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-24 16:24:22 +0700

    Update contact and social media links in AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -7,25 +7,48 @@
         <div class="md:flex">
           <div>
             <h2 class="text-2xl font-bold text-primary-900">Get in touch</h2>
-            <p
-              class="text-base text-gray-100 py-4 leading-8 w-72 hover:animate-pulse"
-            >
-              restubumirr@gmail.com
-            </p>
+            <div class="flex">
+              <NuxtLink
+                to="mailto:restubumirr@gmail.com"
+                class="text-base text-gray-100 py-4 leading-8 w-72 hover:animate-pulse"
+                target="_blank"
+                >restubumirr@gmail.com
+              </NuxtLink>
+            </div>
           </div>
           <div class="">
             <h2 class="text-2xl font-semibold text-primary-900">Connect</h2>
             <ul class="grid leading-8 py-4 text-base text-white">
-              <NuxtLink to="" class="hover:animate-pulse">LinkedIn</NuxtLink>
-              <NuxtLink to="" class="hover:animate-pulse">Discord</NuxtLink>
-              <NuxtLink to="" class="hover:animate-pulse">Instagram</NuxtLink>
-              <NuxtLink to="" class="hover:animate-pulse">Facebook</NuxtLink>
+              <NuxtLink
+                to="https://www.linkedin.com/in/rstubryan/"
+                class="hover:animate-pulse"
+                target="_blank"
+                >LinkedIn</NuxtLink
+              >
+              <NuxtLink
+                to="https://discordapp.com/users/554985727327207424"
+                class="hover:animate-pulse"
+                target="_blank"
+                >Discord</NuxtLink
+              >
+              <NuxtLink
+                to="https://www.instagram.com/zedexzs/"
+                class="hover:animate-pulse"
+                target="_blank"
+                >Instagram</NuxtLink
+              >
+              <NuxtLink
+                to="https://www.facebook.com/hideyoshi.hideyoshi.3386"
+                class="hover:animate-pulse"
+                target="_blank"
+                >Facebook</NuxtLink
+              >
             </ul>
           </div>
         </div>
         <div class="md:flex gap-10">
           <div>
-            <h2 class="text-2xl font-semibold text-primary-900">Blog</h2>
+            <h2 class="text-2xl font-semibold text-primary-900">Blog (Soon)</h2>
             <ul class="grid leading-8 py-4 text-base text-white">
               <NuxtLink to="" class="hover:animate-pulse">Nuxt 3</NuxtLink>
               <NuxtLink to="" class="hover:animate-pulse">Vue 3</NuxtLink>
