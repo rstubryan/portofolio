d2cd39a656be9cd72d9388c58013ccbb6791f3f6
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-26 09:31:43 +0700

    Refactor AboutSection.vue component

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -3,7 +3,9 @@
     <div class="container mx-auto md:h-96 md:px-0 px-8 md:py-0 py-8" id="about">
       <div class="text-center py-4">
         <h2 class="md:text-5xl text-3xl font-bold text-primary">About</h2>
-        <h3 class="md:text-lg text-base font-semibold py-6 text-gray-600 dark:text-gray-400">
+        <h3
+          class="md:text-lg text-base font-semibold py-6 text-gray-600 dark:text-gray-400"
+        >
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quas
           consequuntur veritatis rerum fugit facere nam illum ex, rem cumque
           omnis tempore quasi. Blanditiis nesciunt omnis, sed quas incidunt
