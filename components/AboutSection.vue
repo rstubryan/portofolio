98086a8eaa0e6a18c88614dee4d5e9530a7c3684
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-26 10:12:07 +0700

    Refactor AboutSection component

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    <div class="container mx-auto md:h-96 md:px-0 px-8 md:py-0 py-8" id="about">
-      <div class="text-center py-4">
-        <h2 class="md:text-5xl text-3xl font-bold text-primary">About</h2>
-        <h3
-          class="md:text-lg text-base font-semibold py-6 text-gray-600 dark:text-gray-400"
-        >
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quas
-          consequuntur veritatis rerum fugit facere nam illum ex, rem cumque
-          omnis tempore quasi. Blanditiis nesciunt omnis, sed quas incidunt
-          maxime!
-        </h3>
+    <div
+      class="container mx-auto md:h-96 md:px-0 px-8 md:py-0 py-8 md:flex items-center"
+    >
+      <div class="w-full md:w-[700px] mx-auto">
+        <div class="text-center md:text-start">
+          <h2 class="md:text-5xl text-3xl font-bold text-primary py-8">
+            About me
+          </h2>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="text-justify md:w-full">
+          <p
+            class="md:text-lg text-base font-semibold py-4 text-gray-600 dark:text-gray-400"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+            voluptas necessitatibus voluptate aut magnam? Et, sint laudantium
+            blanditiis aliquam omnis nemo non! Rem, itaque? Autem cumque ducimus
+            repellat facilis ipsam.
+          </p>
+          <p
+            class="md:text-lg text-base font-semibold py-4 text-gray-600 dark:text-gray-400"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+            voluptas necessitatibus voluptate aut magnam? Et, sint laudantium
+            blanditiis aliquam omnis nemo non! Rem, itaque? Autem cumque ducimus
+            repellat facilis ipsam.
+          </p>
+        </div>
       </div>
     </div>
   </div>
